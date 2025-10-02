17d6ccfccfba1587ee5125cc8a2e13ba75b11d00
masud <mohdabbakarr@gmail.com>
2025-10-02 21:46:36 +0100

    ....

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -178,8 +178,6 @@ export default function MyApp({ Component, pageProps }) {
       >
         <ScrollToTopButton />
       </Stack>
-
-      {/* Vercel's Components */}
       <Analytics />
     </Provider>
   );
